656f92992a8dcd8240ee130a2418d17208493e7a
johnBeGoode <jonathan.martin084@gmail.com>
2024-02-06 14:56:47 +0100

    Ajout validation champs formulaire

--- src/Form/WifiParamsType.php
+++ src/Form/WifiParamsType.php
@@ -5,16 +5,25 @@ namespace App\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class WifiParamsType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('networkName', TextType::class)
+            ->add('networkName', TextType::class, [
+                'required' => false,
+                'constraints' => [
+                    new NotBlank(),
+                    new Length(['min' => 5])
+                ]
+            ])
             ->add('encryption', ChoiceType::class, [
                 'choices' => [
                     'WEP' => 'WEP',
@@ -23,7 +32,13 @@ class WifiParamsType extends AbstractType
                 ],
                 'help' => 'The type of security protocol on your network'
             ])
-            ->add('password', TextType::class)
+            ->add('password', PasswordType::class, [
+                'required' => false,
+                'constraints' => [
+                    new NotBlank(),
+                    new Length(['min' => 6])
+                ]
+            ])
             ->add('isHidden', CheckboxType::class, [
                 'label' => 'Hidden wiFi network',
                 'required' => false
